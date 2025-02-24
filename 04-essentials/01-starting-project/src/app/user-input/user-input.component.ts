863113ff30b3979d0c8a2ce609c4408e4b8f72e9
cpiguaveo <cpiguave@conecta.com.ec>
2025-02-24 16:15:45 -0500

    Invest calculator

---
+++ 04-essentials/01-starting-project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  onSubmit() {
+    console.log('Submit');
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+    });
+  }
+}
